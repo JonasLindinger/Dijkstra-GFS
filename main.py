e0e608c364d81f52edbb2f4c8c9033655394da76
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2025-09-27 13:21:55 +0200

    add: content viewing basics

--- main.py
+++ main.py
@@ -2,5 +2,28 @@ from manim import *
 
 class DijkstraIntro(Scene):
     def construct(self):
-        text = Text("Dijkstra's Algorithm")
-        self.play(Write(text))
+        # Show topic
+        title = Text("Dijkstra Algorithmus").scale(1.5)
+        self.play(Write(title, run_time=2))
+        
+        titleAnimation = AnimationGroup(
+            title.animate
+            .scale(0.7)
+            .move_to([-3.2, 3, 0]),
+        )
+
+        self.play(titleAnimation)
+
+        underline = Underline(title, buff=0)
+
+        self.play(Write(underline, run_time=.5))
+
+        # Show content
+        first = Text("1. Was macht der Dijkstra Algorithmus?").scale(.5).move_to([-3.2, 2, 0])
+
+        contentGroup = AnimationGroup(
+            Write(first, run_time=1),
+        )
+
+        self.play(contentGroup)
+        self.wait(5)
